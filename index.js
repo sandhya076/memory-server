5afca2724b7ee8724bfc61aec4563b8b8753e9a5
Sandhya <56761765+sandhya076@users.noreply.github.com>
2022-05-22 10:54:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb",extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb" , extended : true}));
 app.use(cors());
+app.use("/", (req, res) => {
+  res.send("Welcome to memories server hosted from heroku.");
+});
 app.use("/memories",memoriesRoutes);
 
 const CONNECTION_URL = "mongodb+srv://sandhya:Sandhya@cluster0.trov7.mongodb.net/mySecondDatabase?retryWrites=true&w=majority";
